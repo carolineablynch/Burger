5f1fd89a1d7dd6d1c38e7f6816e90ed890793261
Caroline Lynch <carolineablynch@gmail.com>
2021-02-17 00:01:58 -0500

    vscode not pushing to github

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const burgersController = require('./controllers/burgers_controller');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+    layoutsDir: path.join(__dirname, '/views/layouts')
+}));
+app.use('/', burgersController);
+app.listen(port, () => console.log(`App listening on port ${port}`));
