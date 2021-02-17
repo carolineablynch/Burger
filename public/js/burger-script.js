218b0812b6b3d326f521acd74fdc9dc8b7beb182
Caroline Lynch <carolineablynch@gmail.com>
2021-02-16 22:31:53 -0500

    updating the burger script buttons

--- public/js/burger-script.js
+++ public/js/burger-script.js
@@ -1,62 +1,59 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     if (event) {
         console.info("DOM loaded");
-      } 
-    const createBurgerBtn = document.getElementById("create-form");
+      }
 
-    if (createBurgerBtn) {
-        createBurgerBtn.addEventListener("submit", (e) => {
-          e.preventDefault();
-    const newBurger = {
-        name: document.getElementById('ca').value.trim(),
-          };
-    fetch(`/api/burgers/${id}`, {
+const createForm = document.getElementById("create-form");
+createForm.addEventListener('submit', event => { 
+    event.preventDefault();
+
+    const burgerName = document.getElementById('ca');
+    const newBurger = { burger_name: burgerName.value, devoured: false};
+
+    fetch('/api/burgers', {
         method: "POST",
         headers: {
-        Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-
-            body: JSON.stringify(newDevouredState),
-        }).then((res) => {
-
-            document.getElementById("ca").value = '';
-
-            console.log("new burger was made");
-            location.reload();
-          });
-        });
-      }
-
-
-    const changeDevourBtn = document.querySelectorAll(".change-devoured"); 
-    if (changeDevourBtn) {
-        changeDevourBtn.forEach((button) => {
-            button.addEventListener("click", (e) => {
-                const id = e.target.getAttribute("data-id");
-                var devourState = e.target.getAttribute("data-devoured");
-    const newDevouredState = true;
-    console.log(newDevouredState)
-
-    fetch(`/api/burgers/${id}`, {
-        method: 'PUT',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         },
+        body: JSON.stringify(newBurger)
+    }).then(response => {
+        if(response.ok) {
+            burgerName.value = '';
 
-        body: JSON.stringify({devoured: newDevouredState}),
-    }).then((response) => {
-
-            if (response.ok) {
-                console.log(`changed devoured/eaten to: ${newDevouredState}`);
-                location.reload('/');
-              } else {
-                alert('something went wrong!');
-              }
-            });
-          });
-        });
-      }
-    
+            console.log("created new burger!");
+            window.location.reload();
+        }
     });
+})
+
+const changeDevourBtn = document.querySelectorAll(".change-devoured");
+changeDevourBtn.forEach((button) => {
+    button.addEventListener("click" (e) => {
+        const id = e.target.getAttribute("data-id");
+        const devourState = !!parseInt(e.target.getAttribute("data-devoured"));
+        const newDevouredState = !devourState;
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+})
